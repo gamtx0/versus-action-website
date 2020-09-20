59bda80cf48518d3c9ad21b8b375973045a57a30
0xAlchemist <eric@ebdigital.ca>
2020-09-20 16:26:15 -0700

    Move styles from document head to App.scss
    - import from external file
    -- now we can use normalize for browser consistency
    -- provides a base set of styles for the app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './App.scss';
 import './config';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-     <div class="bg-columns"></div>
-     <div class="bg-triangle"></div>
+    <div class="bg-columns"></div>
+    <div class="bg-triangle"></div>
     <App />
   </React.StrictMode>,
   document.getElementById('root')

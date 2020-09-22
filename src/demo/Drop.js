1093bd608a5f763b90fc2f8ba4d8a42aa10876a6
0xAlchemist <eric@ebdigital.ca>
2020-09-21 20:55:33 -0700

    Update center column styles
    - add Artist component
    - various CSS changes

--- src/demo/Drop.js
+++ src/demo/Drop.js
@@ -1,9 +1,17 @@
-import React, {useEffect} from "react"
-import * as fcl from "@onflow/fcl"
-import * as t from "@onflow/types"
-import * as sdk from "@onflow/sdk"
+import React, { useEffect } from "react";
+import * as fcl from "@onflow/fcl";
+import * as t from "@onflow/types";
+import * as sdk from "@onflow/sdk";
 
-import {Art, Image, Title, Description, Remaining, Time } from '../components/ArtDrop'
+import {
+  Art,
+  Image,
+  Title,
+  Artist,
+  Description,
+  Remaining,
+  Time,
+} from "../components/ArtDrop";
 
 const fetchVersusDrop = `
 import Versus from 0x045a1763c93006ca
@@ -26,10 +34,15 @@ pub fun main(address:Address) : Versus.DropStatus?{
     } 
   return nil
 }
-`
+`;
 
-const Drop = ({ marketplaceAccount, drop, handleDrop, bidTransaction, handleBidTransaction}) => {
- 
+const Drop = ({
+  marketplaceAccount,
+  drop,
+  handleDrop,
+  bidTransaction,
+  handleBidTransaction,
+}) => {
   /*
   useEffect(() => {
     const interval = setInterval(() => {
@@ -44,33 +57,32 @@ const Drop = ({ marketplaceAccount, drop, handleDrop, bidTransaction, handleBidT
     async function fetchDrop() {
       const response = await fcl.send([
         fcl.script(fetchVersusDrop),
-        sdk.args([ sdk.arg(marketplaceAccount, t.Address) ])
-      ])
-      const dropResponse=await fcl.decode(response)
-      handleDrop(dropResponse)
-      handleBidTransaction(null) //we mark that the current transaction has been taken into account
+        sdk.args([sdk.arg(marketplaceAccount, t.Address)]),
+      ]);
+      const dropResponse = await fcl.decode(response);
+      handleDrop(dropResponse);
+      handleBidTransaction(null); //we mark that the current transaction has been taken into account
     }
-    if( drop == null || bidTransaction != null) {
-        console.log("FETCH DROP")
-        fetchDrop()
+    if (drop == null || bidTransaction != null) {
+      console.log("FETCH DROP");
+      fetchDrop();
     }
-  }, [drop, marketplaceAccount, bidTransaction])
-
+  }, [drop, marketplaceAccount, bidTransaction]);
 
   return (
-     drop && <Art>  
-          <Image alt="art" src={drop.uniqueStatus.metadata.url} />
-          <Title>{drop.uniqueStatus.metadata.name}</Title>
-          by: <span>{drop.uniqueStatus.metadata.artist}</span> <br/><br/> 
-          <Description href="read">read about the piece...</Description>
+    drop && (
+      <Art>
+        <Image alt="art" src={drop.uniqueStatus.metadata.url} />
+        <Title>{drop.uniqueStatus.metadata.name}</Title>
+        <Artist>by: {drop.uniqueStatus.metadata.artist}</Artist>
+        <Description href="read">Read about the piece...</Description>
 
-          <Remaining>Blocks remaining:</Remaining> 
-          <Time>{drop.uniqueStatus.blocksRemaining}</Time>
-         
-
-    </Art>
-  )
-}
+        <Remaining>Blocks remaining:</Remaining>
+        <Time>{drop.uniqueStatus.blocksRemaining}</Time>
+      </Art>
+    )
+  );
+};
 //this needs to own UniqueAuction and EditionedAuction and bid state must go up here. TO refetch the drop
 
-export default Drop
+export default Drop;

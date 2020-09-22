bf2954b7c4d267044da65cdfcee39a15bfbd0996
0xAlchemist <eric@ebdigital.ca>
2020-09-22 13:01:38 -0700

    Restructure unique auction component
    - remove line breaks
    - restructure HTML

--- src/demo/UniqueAuction.js
+++ src/demo/UniqueAuction.js
@@ -1,60 +1,65 @@
-import React from "react"
+import React from "react";
 
-import Bid from './Bid'
-import Events from './Events'
+import Bid from "./Bid";
+import Events from "./Events";
 
-import {UniqueAuctionBox, Title, Price,  Winning, Loosing, Tie} from '../components/Auction'
+import {
+  UniqueAuctionBox,
+  Title,
+  Subtitle,
+  Label,
+  Price,
+  Winning,
+  Loosing,
+  Tie,
+  History,
+} from "../components/Auction";
 
-const UniqueAuction = ({ drop, marketplaceAccount,  handleBidTransaction, bidTranscation }) => {
-
-  var status= <Winning>Highest bid</Winning>
-  if(drop.winning === "EDITIONED") {
-    status = <Loosing>Lowest bid</Loosing>
-  }else if(drop.winning === "TIE") {
-    status = <Tie>Tied bid</Tie>
+const UniqueAuction = ({
+  drop,
+  marketplaceAccount,
+  handleBidTransaction,
+  bidTranscation,
+}) => {
+  var status = <Winning>Highest bid</Winning>;
+  if (drop.winning === "EDITIONED") {
+    status = <Loosing>Lowest bid</Loosing>;
+  } else if (drop.winning === "TIE") {
+    status = <Tie>Tied bid</Tie>;
   }
 
   return (
     <UniqueAuctionBox>
-   
-          <Title>Own the 1/1 NFT</Title>
-          Current bid:<br /> 
-          <Price>
-          {drop.uniquePrice} FT
-          </Price>
-          <br />
-          <br />
-          <br />
-         { status }
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <Bid 
-            marketplaceAccount={marketplaceAccount} 
-            dropId={drop.dropId} 
-            auctionId={drop.uniqueStatus.id} 
-            minNextBid={drop.uniqueStatus.minNextBid}
-            handleBidTransaction={handleBidTransaction}  />
-          <br />
-           bid history: {drop.uniqueStatus.bids}
-          <br />
-          <br />
+      <Title>Own the 1/1 NFT</Title>
+      <Subtitle>
+        <span>*Plus</span> the original painting
+      </Subtitle>
+      <Label>Current bid:</Label>
+      <Price>{drop.uniquePrice} FT</Price>
+      {status}
 
-          {  <Events 
-          startBlock={drop.uniqueStatus.startBlock}
-          dropId={drop.dropId}
-          auctionId={drop.uniqueStatus.id}
-          bidTranscation={bidTranscation}
-          /> }
-      </UniqueAuctionBox>
-   )
-}
+      <Bid
+        marketplaceAccount={marketplaceAccount}
+        dropId={drop.dropId}
+        auctionId={drop.uniqueStatus.id}
+        minNextBid={drop.uniqueStatus.minNextBid}
+        handleBidTransaction={handleBidTransaction}
+      />
+      <History>
+        <Label>
+          bid history: <span>{drop.uniqueStatus.bids}</span>
+        </Label>
+        {
+          <Events
+            startBlock={drop.uniqueStatus.startBlock}
+            dropId={drop.dropId}
+            auctionId={drop.uniqueStatus.id}
+            bidTranscation={bidTranscation}
+          />
+        }
+      </History>
+    </UniqueAuctionBox>
+  );
+};
 
-export default UniqueAuction
+export default UniqueAuction;

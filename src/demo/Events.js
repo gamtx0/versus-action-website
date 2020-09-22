cfc2c08f42493888e4f3eb8be6d26ca176cde3a3
0xAlchemist <eric@ebdigital.ca>
2020-09-22 13:00:21 -0700

    Update table styles
    - various tweaks to match figma design

--- src/demo/Events.js
+++ src/demo/Events.js
@@ -1,62 +1,95 @@
-import React, {useState, useEffect} from "react"
-import * as sdk from "@onflow/sdk"
-import * as fcl from "@onflow/fcl"
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import * as sdk from "@onflow/sdk";
+import * as fcl from "@onflow/fcl";
 
+const GetEvents = ({ startBlock, dropId, auctionId, bidTransaction }) => {
+  const [result, setResult] = useState(null);
 
-const GetEvents = ({startBlock, dropId, auctionId, bidTransaction}) => {
-  const [result, setResult] = useState(null)
-
-  const eventType="A.045a1763c93006ca.Versus.Bid"
+  const eventType = "A.045a1763c93006ca.Versus.Bid";
 
   useEffect(() => {
     async function fetchEvent() {
-      const url = await fcl.config().get("accessNode.api")
-      const response = await sdk.send(await sdk.build([
-        sdk.getEvents(eventType, startBlock, null),
-      ]), { node: url })
-  
-      const decodedResponse = await sdk.decodeResponse(response)
+      const url = await fcl.config().get("accessNode.api");
+      const response = await sdk.send(
+        await sdk.build([sdk.getEvents(eventType, startBlock, null)]),
+        { node: url }
+      );
+
+      const decodedResponse = await sdk.decodeResponse(response);
       const filtered = decodedResponse
-      .filter(result => result.data.dropId === dropId && result.data.auctionId === auctionId )
-      .map(result => ({
-        amount: result.data.bidPrice,
-        address: result.data.bidderAddress,
-        blockHeight: result.data.blockHeight, 
-        date: new Date(result.data.time * 1000 ).toLocaleString(),
-        unixTime: result.data.time
-      })).reverse()
+        .filter(
+          (result) =>
+            result.data.dropId === dropId && result.data.auctionId === auctionId
+        )
+        .map((result) => ({
+          amount: result.data.bidPrice,
+          address: result.data.bidderAddress,
+          blockHeight: result.data.blockHeight,
+          date: new Date(result.data.time * 1000).toLocaleString(),
+          unixTime: result.data.time,
+        }))
+        .reverse();
 
-
-      setResult(filtered)
-      
+      setResult(filtered);
     }
-    fetchEvent()
-  }, [eventType, startBlock, dropId, auctionId, bidTransaction ])
+    fetchEvent();
+  }, [eventType, startBlock, dropId, auctionId, bidTransaction]);
 
-  const resultNotEmpty = result && result.length > 0
+  const shortenAddress = (address) => {
+    let addressPrefix = address.substr(0, 5);
+    let addressSuffix = address.substr(address.length - 3, address.length);
+
+    return addressPrefix + "..." + addressSuffix;
+  };
+
+  const BidHistory = styled.table`
+    padding: 0;
+
+    th {
+      border-bottom: none;
+      font-weight: 400;
+    }
+
+    th,
+    td {
+      font-size: 14px;
+      line-height: 16px;
+      overflow: hidden;
+      text-align: left;
+      width: 25%;
+
+      &:nth-of-type(2) {
+        width: 50%;
+        padding: 0 10px;
+      }
+    }
+  `;
+
+  const resultNotEmpty = result && result.length > 0;
   return (
     <div>
-      { resultNotEmpty && 
-      <table>
-        <thead>
-          <tr>
-          <th>Address</th>
-          <th>Time</th>
-          <th>Bid</th>
-          </tr>
-        </thead>
-        <tbody>
-        { result.map( it =>
-            <tr key={it.date}>
-              <td>{it.address}</td>
-              <td>{it.date}</td>
-              <td>{it.amount}</td>
+      {resultNotEmpty && (
+        <BidHistory>
+          <thead>
+            <tr>
+              <th>Address</th>
+              <th>Time</th>
+              <th>Bid</th>
             </tr>
-          )}
+          </thead>
+          <tbody>
+            {result.map((it) => (
+              <tr key={it.date}>
+                <td>{shortenAddress(it.address)}</td>
+                <td>{it.date}</td>
+                <td>{it.amount}</td>
+              </tr>
+            ))}
           </tbody>
-      </table>
-      }
+        </BidHistory>
+      )}
     </div>
-  )
-}
-export default GetEvents
+  );
+};
+export default GetEvents;
